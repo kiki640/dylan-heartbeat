584d65e471bbab0fe2b44386bb52ce76d8b71a67
kiki640 <2914673285@qq.com>
2026-09-05 09:50:00 +0800

    Update wake_up.js

--- wake_up.js
+++ wake_up.js
@@ -415,7 +415,7 @@ async function runWakeUp() {
 
   const now = new Date();
   const diffMinutes = Math.floor((now - lastUserTime) / 1000 / 60);
-
+  console.log("lastUserTime:", lastUserTime, "now:", new Date(), "diff:", Math.floor((new Date() - lastUserTime) / 60000));
   if (!shouldWake(lastUserTime)) {
     console.log("\n暂不需要唤醒\n");
     return;
